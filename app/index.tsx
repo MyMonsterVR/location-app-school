56bf121e50c9daa6b37520221797ea5d62a6da81
MyMonsterVR <mymonsterdk@gmail.com>
2024-10-22 12:36:12 +0200

    MapView works

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,94 @@
-import { Text, View } from "react-native";
+import { useState, useEffect, useRef } from 'react';
+import { Text, View, StyleSheet, Platform } from "react-native";
+import MapViewDirections from 'react-native-maps-directions';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import * as Location from 'expo-location';
+import Constants from 'expo-constants';
+/*
+const origin = {latitude: 37.3318456, longitude: -122.0296002};
+const destination = {latitude: 37.771707, longitude: -122.4053769};
+const GOOGLE_MAPS_APIKEY = 'AIzaSyDVv71QIEt1ymIvRM-VgiDSg7p1bToQ864';
+ */
+
+
+
+/* <MapViewDirections
+          origin={origin}
+          destination={destination}
+          apikey={GOOGLE_MAPS_APIKEY}
+        /> */
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+    const [location, setLocation] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
+    const mapRef = useRef(null);
+
+    useEffect(() => {
+        (async () => {
+            /* @hide */
+            /* if (Platform.OS === 'android' && !Constants.isDevice) {
+                setErrorMsg(
+                    'Oops, this will not work on Snack in an Android emulator. Try it on your device!'
+                );
+                return;
+            } */
+            /* @end */
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+            if (location?.coords)
+            {
+                const region = {
+                    latitudeDelta:  0.0092,
+                    longitudeDelta: 0.0092,
+                    latitude:       location.coords.latitude,
+                    longitude:      location.coords.longitude,
+                };
+
+                console.log("latitude: ", region.latitude);
+                console.log("longitude: ", region.longitude);
+                mapRef.current?.animateToRegion(region, 1000);
+            }
+        })();
+      }, []);
+
+    let text = 'Waiting..';
+    if (errorMsg)
+    {
+        text = errorMsg;
+        return (
+            <View>
+                <Text>Location not found</Text>
+                <Text>{text}</Text>
+                <Text>{JSON.stringify(location)}</Text>
+            </View>
+        )
+    }
+    else if (location)
+    {
+        text = JSON.stringify(location);
+    }
+
+    return (
+        <View style={styles.container} provider={PROVIDER_GOOGLE}>
+            <Text>{text}</Text>
+            <MapView ref={mapRef} style={styles.map}></MapView>
+        </View>
+    );
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    map: {
+        width: '100%',
+        height: '100%',
+    },
+});
