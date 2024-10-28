5adb363df7c55516639e85777d42def89a2d3fd2
William Andersen <45121289+williloft@users.noreply.github.com>
2024-10-28 10:40:23 +0100

    added taps the app

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,22 @@
 import { Tabs } from 'expo-router';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+
 
 export default () => {
     return (
         <Tabs screenOptions={{ headerShown: false }}>
-            <Tabs.Screen name="friends" />
-            <Tabs.Screen name="map" />
-            <Tabs.Screen name="settings" />
+            <Tabs.Screen name="friends" options={{
+                title: 'Friends',
+                tabBarIcon: ({ color }) => <FontAwesome size={28} name="users" color="gray" />,
+            }}/>
+            <Tabs.Screen name="map" options={{
+                title: 'Map',
+                tabBarIcon: ({ color }) => <FontAwesome size={28} name="map" color="gray" />,
+            }}/>
+            <Tabs.Screen name="settings" options={{
+                title: 'Settings',
+                tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color="gray" />,
+            }}/>
         </Tabs>
     );
 };
