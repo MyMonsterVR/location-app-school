822d086f183002a21c6f715deb3dd7739f4c516a
Rasmus Kristensen <mymonsterdk@gmail.com>
2024-10-30 09:34:37 +0100

    Created utils file with isLoggedIn checker function

---
+++ app/utils/utils.js
@@ -0,0 +1,24 @@
+import {getItem} from "./AsyncStorage";
+
+class Utils {
+
+    isLoggedIn() {
+        async function isTokenValid(user) {
+            const response = await fetch('http://20.157.195.19/api/validateToken', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: { token: await getItem('token') },
+            });
+
+            // TODO: REMOVE
+            if(!response.ok) return true;
+
+            return response.json().valid;
+        }
+
+        return isTokenValid();
+    }
+
+}
