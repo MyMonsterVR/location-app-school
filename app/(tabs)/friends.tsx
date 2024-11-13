f2607e2d8876d63e1ab7c02f915a535e926b8d3f
MyMonsterVR <mymonsterdk@gmail.com>
2024-11-13 12:57:54 +0100

    Chat gifs works now, needs to be optimized

--- app/(tabs)/friends.tsx
+++ app/(tabs)/friends.tsx
@@ -1,92 +1,114 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { ScrollView, Pressable, TextInput, StyleSheet, View, Modal } from 'react-native';
+import { Image } from 'expo-image';
+import {ScrollView, Pressable, TextInput, StyleSheet, View, Modal, FlatList} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import Icon from "react-native-vector-icons/Ionicons";
+import Icon from 'react-native-vector-icons/Ionicons';
 import { Text } from '~/components/ui/text';
-import { useAuth } from '@clerk/clerk-expo';
+import { useAuth, useUser } from '@clerk/clerk-expo';
 
 const SERVER_URL = 'http://20.157.195.19';
 
 // Define types for message and user data
 interface User {
     username: string;
-    userId: string;  // User ID should be a string (could be numeric or UUID)
+    userId: string; // User ID should be a string (could be numeric or UUID)
 }
 
 interface Message {
     userId: string;
     text: string;
-    type: string;
+    messageType: 'text' | 'gif';
     username?: string;
     sentByClient?: boolean; // Flag to differentiate client messages
 }
 
 // Utility function to create a room ID dynamically
 const createRoomId = (userIds: string[]): string => {
-    // Sort user IDs to ensure consistent room ID generation
     const sortedUserIds = userIds.sort();
     return `room-${sortedUserIds.join('-')}`;
 };
 
 const Friends: React.FC = () => {
     const [messages, setMessages] = useState<Message[]>([]);
-    const [message, setMessage] = useState<string>('');
+    const [message, setMessage] = useState<string>(''); // message text state
     const [friends, setFriends] = useState<User[]>([
         { username: 'willer fake', userId: 'user_2oWHzUce33wlLHl4taHPaYpeIYn' },
         { username: 'raller fake', userId: 'user_2oWWxEKyYTXW1HD21yPggkAlYjx' },
         { username: 'test3', userId: 'user3' },
     ]);
+    const [gifSearch, setGifSearch] = useState<string>('');
+    const [categories, setCategories] = useState<any[]>([]);
+    const [isGifModalVisible, setIsGifModalVisible] = useState<boolean>(false);
+    const [gifs, setGifs] = useState<any[]>([]);
     const [modalVisible, setModalVisible] = useState<boolean>(false);
     const [roomId, setRoomId] = useState<string>('');
-    const { userId } = useAuth();  // Assuming Clerk's useAuth hook provides the current user ID
+    const { userId } = useAuth();
+    const { user } = useUser();
 
-    let ws = useRef<WebSocket | null>(null).current;
-
-    console.log(userId)
+    let ws = useRef<WebSocket | null>(null);
 
     useEffect(() => {
-        if (!userId || !roomId) return; // Wait until userId and roomId are available
+        if (!userId || !roomId) return;
 
-        // Initialize WebSocket connection if it's not already established
-        if (!ws) {
-            ws = new WebSocket(`${SERVER_URL}:8080/chat`);
+        if (!ws.current) {
+            ws.current = new WebSocket(`${SERVER_URL}:8080/chat`);
         }
 
-        ws.onopen = () => {
+        const socket = ws.current;
+
+        socket.onopen = () => {
             console.log('Connected to WebSocket server');
-            // Join the room based on the dynamic roomId and userId
-            ws.send(JSON.stringify({ type: 'join', room: roomId, userId }));
+            socket.send(JSON.stringify({ type: 'join', room: roomId, userId, displayName: user?.username }));
         };
 
-        ws.onmessage = (e) => {
+        socket.onmessage = (e) => {
             const data = JSON.parse(e.data);
             if (data.type === 'history') {
-                setMessages(data.messages); // Fetch history on connect
+                setMessages(data.messages);
             } else if (data.type === 'message' && data.sentByClient !== true) {
                 setMessages((prevMessages) => [...prevMessages, data]);
             }
         };
 
         return () => {
-            // Optionally close WebSocket connection when the component is unmounted or roomId changes
-            // ws.close();
+            socket.close();
         };
-    }, [userId, roomId]); // Reconnect when userId or roomId changes
+    }, [userId, roomId, user]);
+
 
     const scrollViewRef = useRef<ScrollView | null>(null);
 
     useEffect(() => {
         if (scrollViewRef.current) {
-            scrollViewRef.current.scrollToEnd({ animated: true });
+            scrollViewRef.current.scrollToEnd({ animated: false });
         }
     }, [messages]); // This will scroll to the bottom whenever messages change
 
+    useEffect(() => {
+        const fetchCategories = async () => {
+            try {
+                const response = await fetch(
+                    'https://tenor.googleapis.com/v2/categories?key=' +
+                    process.env.EXPO_PUBLIC_TENOR_KEY +
+                    '&client_key=my_test_app'
+                );
+                const data = await response.json();
+                setCategories(data.tags);
+            } catch (error) {
+                console.error('Error fetching categories:', error);
+            }
+        };
+
+        if (isGifModalVisible) {
+            fetchCategories();
+        }
+    }, [isGifModalVisible]);
+
     // Handle the room selection (either one-on-one or group chat)
     const onSelectChat = (selectedUsers: User[]) => {
-        // Create room ID based on the selected users
-        const userIds = selectedUsers.map(user => user.userId);
+        const userIds = selectedUsers.map((user) => user.userId);
         if (userId && !userIds.includes(userId)) {
-            userIds.push(userId);  // Ensure the current user is included in the room
+            userIds.push(userId); // Ensure the current user is included in the room
         }
 
         const newRoomId = createRoomId(userIds);
@@ -95,8 +117,11 @@ const Friends: React.FC = () => {
     };
 
     // Send a message to the backend and update state
-    const sendMessage = () => {
-        if (!message.trim()) return;
+    const sendMessage = (text: string, messageType: 'text' | 'gif') => {
+        if (!text.trim()) return;
+
+        // Clear the input immediately
+        setMessage('');
 
         // Send message via HTTP request
         fetch(`${SERVER_URL}/send`, {
@@ -107,21 +132,82 @@ const Friends: React.FC = () => {
             body: JSON.stringify({
                 room: roomId,
                 userId: userId ?? '', // Ensure userId is included
-                text: message,
+                username: user?.username,
+                text,
+                messageType,
             }),
         })
             .then((response) => response.json())
             .then((data) => {
-                console.log('Message sent:', data);
                 setMessages((prevMessages) => [
                     ...prevMessages,
-                    { userId: userId ?? '', text: message, type: 'message', sentByClient: true },
+                    { userId: userId ?? '', text, messageType, sentByClient: true },
                 ]);
-                setMessage(''); // Clear input after sending
             })
             .catch((error) => {
                 console.error('Error sending message:', error);
             });
+    };
+
+    function debounce(func: Function, delay: number) {
+        let timer: NodeJS.Timeout;
+
+        return (...args: any[]) => {
+            if (timer) {
+                clearTimeout(timer); // Cancel the previous timer if the user keeps typing
+            }
+
+            timer = setTimeout(() => {
+                func(...args); // Call the function after the delay
+            }, delay);
+        };
+    }
+
+    const debouncedSearchGifs = useRef(
+        debounce(async (searchTerm: string) => {
+            if (!searchTerm.trim()) return;
+
+            const searchUrl = `https://tenor.googleapis.com/v2/search?q=${searchTerm}&key=${process.env.EXPO_PUBLIC_TENOR_KEY}&client_key=my_test_app&limit=10`;
+
+            try {
+                const response = await fetch(searchUrl);
+                const data = await response.json();
+                setGifs(data.results); // Update the state with the GIF results
+            } catch (error) {
+                console.error('Error fetching GIFs:', error);
+            }
+        }, 500) // 500ms debounce
+    ).current;
+
+    useEffect(() => {
+        debouncedSearchGifs(gifSearch);
+    }, [gifSearch]);
+
+    const fetchGifsByCategory = (category: string) => {
+        const searchUrl = `https://tenor.googleapis.com/v2/search?q=${category}&key=${process.env.EXPO_PUBLIC_TENOR_KEY}&client_key=my_test_app&limit=10`;
+
+        fetch(searchUrl)
+            .then((response) => response.json())
+            .then((data) => {
+                setGifs(data.results); // Update the state with the GIF results
+            })
+            .catch((error) => {
+                console.error('Error fetching GIFs:', error);
+            });
+    };
+
+    const closeGifModal = () => {
+        setIsGifModalVisible(false);
+        setGifs([]);
+        setGifSearch('');
+    };
+
+    const insertGif = (gif: any) => {
+        const gifUrl = gif.media_formats.gif.url;
+
+        // Immediately send the GIF URL to the server, without modifying the message state
+        sendMessage(gifUrl, 'gif');
+        closeGifModal()
     };
 
     return (
@@ -138,7 +224,6 @@ const Friends: React.FC = () => {
             {/* Modal with Navbar and Chat Area */}
             <Modal visible={modalVisible} animationType="slide">
                 <View style={styles.modalContainer}>
-                    {/* Navbar */}
                     <View style={styles.navbar}>
                         <Pressable onPress={() => setModalVisible(false)}>
                             <Icon name="arrow-back" size={24} color="#fff" />
@@ -148,50 +233,106 @@ const Friends: React.FC = () => {
 
                     {/* Chat Area */}
                     <View style={styles.chatArea}>
-                        <ScrollView ref={scrollViewRef} contentContainerStyle={styles.messagesContainer}>
-                            {messages.map((data, index) => {
-                                const showUsername = index === 0 || messages[index - 1].userId !== data.userId;
+                        <FlatList
+                            data={messages}
+                            keyExtractor={(item, index) => index.toString()}
+                            renderItem={({ item, index }) => {
+                                const showUsername = index === 0 || messages[index - 1].userId !== item.userId;
                                 return (
-                                    <View
-                                        key={index}
-                                        style={[
-                                            styles.messageContainer,
-                                            data.userId === userId ? styles.userMessage : styles.otherUserMessage
-                                        ]}
-                                    >
-                                        {data.userId !== userId && showUsername && (
+                                    <View style={[styles.messageContainer, item.userId === userId ? styles.userMessage : styles.otherUserMessage]}>
+                                        {item.userId !== userId && showUsername && (
                                             <View style={styles.messageHeader}>
-                                                <Text style={styles.username}>{data.username || 'Unknown'}</Text>
+                                                <Text style={styles.username}>{item.username || 'Unknown'}</Text>
                                             </View>
                                         )}
-
-                                        {data.userId === userId && showUsername && (
+                                        {item.userId === userId && showUsername && (
                                             <View style={styles.messageHeaderRight}>
-                                                <Text style={styles.username}>{data.username || 'Me'}</Text>
+                                                <Text style={styles.username}>{'Me'}</Text>
                                             </View>
                                         )}
-
-                                        {/* Message Text */}
-                                        <Text style={styles.messageText}>{data.text}</Text>
+                                        {item.messageType === 'text' ? (
+                                            <Text style={styles.messageText}>{item.text}</Text>
+                                        ) : (
+                                            <Image source={{ uri: item.text }} style={styles.gifMessage} />
+                                        )}
                                     </View>
                                 );
-                            })}
-                        </ScrollView>
+                            }}
+                            contentContainerStyle={styles.messagesContainer}
+                            onContentSizeChange={() => scrollViewRef.current?.scrollToEnd({ animated: false })}
+                        />
+
                     </View>
 
                     {/* Message Input with Send Button */}
                     <View style={styles.inputContainer}>
+                        <Pressable onPress={() => setIsGifModalVisible(true)} style={styles.gifButton}>
+                            <Icon name="image" size={24} color="#fff" />
+                        </Pressable>
                         <TextInput
                             value={message}
                             onChangeText={setMessage}
                             placeholder="Type a message..."
-                            onSubmitEditing={sendMessage}
+                            onSubmitEditing={() => sendMessage(message, 'text')}
                             style={styles.input}
                         />
-                        <Pressable onPress={sendMessage} style={styles.sendButton}>
+                        <Pressable onPress={() => sendMessage(message, 'text')} style={styles.sendButton}>
                             <Icon name="send" size={24} color="#0078d4" />
                         </Pressable>
                     </View>
+                </View>
+            </Modal>
+
+            {/* GIF Modal */}
+            <Modal visible={isGifModalVisible} animationType="slide">
+                <View style={styles.modalContainer}>
+                    <View style={styles.gifHeader}>
+                        <TextInput
+                            value={gifSearch}
+                            onChangeText={setGifSearch}
+                            placeholder="Search for GIFs"
+                            style={styles.searchInput}
+                        />
+                        <Pressable onPress={() => closeGifModal()} style={styles.closeButton}>
+                            <Icon name="close" size={24} color="#fff" />
+                        </Pressable>
+                    </View>
+
+                    {/* Categories */}
+                    {!gifSearch && (
+                        <ScrollView horizontal style={styles.categoriesContainer}>
+                            {categories.map((category) => (
+                                <Pressable key={category.searchterm} onPress={() => fetchGifsByCategory(category.searchterm)}>
+                                    <Text style={styles.categoryText}>{category.name}</Text>
+                                </Pressable>
+                            ))}
+                        </ScrollView>
+                    )}
+
+                    {/* GIF Thumbnails */}
+                    <ScrollView contentContainerStyle={styles.gifsContainer}>
+                        {gifs.map((gif, index) => {
+                            const isTallGif = gif.media_formats.gif.width < gif.media_formats.gif.height;
+                            return (
+                                <Pressable
+                                    key={index}
+                                    onPress={() => insertGif(gif)}
+                                    style={[
+                                        styles.gifThumbnailContainer,
+                                        isTallGif && styles.tallGifThumbnail,
+                                    ]}
+                                >
+                                    <Image
+                                        source={{ uri: gif.media_formats.gif.url }}
+                                        style={[
+                                            styles.gifThumbnail,
+                                            isTallGif && styles.tallGifThumbnailImage,
+                                        ]}
+                                    />
+                                </Pressable>
+                            );
+                        })}
+                    </ScrollView>
                 </View>
             </Modal>
         </SafeAreaView>
@@ -305,6 +446,64 @@ const styles = StyleSheet.create({
         marginLeft: 10,
         padding: 8,
         borderRadius: 50,
+    },
+    searchInput: {
+        flex: 1,
+        backgroundColor: '#fff',
+        borderRadius: 15,
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+        fontSize: 16,
+    },
+    closeButton: {
+        padding: 10,
+    },
+    gifButton: {
+        padding: 8,
+        marginLeft: 10,
+        borderRadius: 50,
+    },
+    gifHeader: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 10,
+        backgroundColor: '#333',
+    },
+    gifMessage: {
+        width: 200, // Adjust the size based on your needs
+        height: 200,
+        borderRadius: 10,
+    },
+    gifsContainer: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        padding: 10,
+    },
+    gifThumbnailContainer: {
+        width: '48%',
+        margin: '1%',
+    },
+    gifThumbnail: {
+        width: '100%',
+        height: 150,
+        borderRadius: 10,
+        resizeMode: 'cover',
+    },
+    tallGifThumbnail: {
+        width: '100%',
+        height: 200,
+    },
+    tallGifThumbnailImage: {
+        height: '100%',
+        resizeMode: 'cover',
+    },
+    categoriesContainer: {
+        padding: 10,
+    },
+    categoryText: {
+        color: '#fff',
+        padding: 10,
+        fontSize: 18,
     },
 });
 
